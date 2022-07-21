585c3e0b413c08ed13367573ac0f6be63fcc1e57
cis <rahul@intuitionsoftech.com>
2022-07-21 23:04:51 +0530

    notification list api updated

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -15,6 +15,6 @@ class Notification extends Model
     ];
 
     public function user(){
-        return $this->belongsTo(\App\Models\User::class);
+        return $this->belongsTo(\App\Models\User::class)->select('name' , 'last_name','email','mobile_no','country_code');
     }
 }
